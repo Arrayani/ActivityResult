399f8a38934b3cdc4fc2642707194c02652670a8
Roni <rampage9198@yahoo.com>
2022-05-28 16:11:44 +0700

    commit

--- app/src/main/java/com/gamecodeschool/activityresult/ThirdActivity.kt
+++ app/src/main/java/com/gamecodeschool/activityresult/ThirdActivity.kt
@@ -3,6 +3,9 @@ package com.gamecodeschool.activityresult
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import com.gamecodeschool.activityresult.databinding.ActivityThirdBinding
 
 class ThirdActivity : AppCompatActivity() {
@@ -13,20 +16,27 @@ class ThirdActivity : AppCompatActivity() {
         //setContentView(R.layout.activity_third)
         binding = ActivityThirdBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        // kurang huruf s nya udah ga bisa ini ActivityResultContracts
+        val getImage  = registerForActivityResult(
+            ActivityResultContracts.GetContent(),
+        ActivityResultCallback {
+            binding.imageView.setImageURI(it)
+        })
         binding.button.setOnClickListener{
-            val intent = Intent()
-            intent.type =  "image/"
-            intent.action = Intent.ACTION_GET_CONTENT
-            startActivityForResult(Intent.createChooser(intent,"select the image"),100)
+
+        getImage.launch("image/")
+//            val intent = Intent()
+//            intent.type =  "image/"
+//            intent.action = Intent.ACTION_GET_CONTENT
+//            startActivityForResult(Intent.createChooser(intent,"select the image"),100)
         }
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == 100)
-            if (data!=null)
-                binding.imageView.setImageURI(data.data)
-    }
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        super.onActivityResult(requestCode, resultCode, data)
+//        if (requestCode == 100)
+//            if (data!=null)
+//                binding.imageView.setImageURI(data.data)
+//    }
 
 }
